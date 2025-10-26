c2c8ca393fe3f75c49a965e80360af1470e94c5a
Marcel Szymanek <marcelszymanek2@gmail.com>
2025-10-26 15:19:03 +0100

    zadaniee

---
+++ lab03/zadanie4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()[
+    float a, b, c;
+    printf("Podaj trzy liczby: ");
+    scanf("%d, %d, %d" &a, &b, &c);
+
+    if (a > b, c) {
+    printf("Największa: %d\n", a);
+    }
+if (b > a, c) {
+printf("Największa: %d\n", b);
+}
+else {
+    printf("Największa: %d\n", c);
+}
+    ]
