7b8f1b91c0712789054d410c92344130f6a629d7
Marcel Szymanek <marcelszymanek2@gmail.com>
2025-10-26 14:44:25 +0100

    zadaniee

---
+++ lab03/zadanie3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    printf("Podaj dwie liczby: ");
+    scanf("%d %d", %a, %b);
+
+    if (a > b)
+    printf("Większa liczba: %d\n", a);
+    else
+    printf("Większa liczba: %d\n", b);
+}
