3fe6eebc1cc5ccffa6ed6c039f8e5611a9331ebf
Marcel Szymanek <marcelszymanek2@gmail.com>
2025-11-23 14:33:20 +0100

    zadaniee

---
+++ lab03/zadanie3Lab05.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int liczba(int a, int b, int c) {
+    int d;
+    for (d = a; e++) {
+        int potega = 1;
+
+        for (int i = 0; i < c; i++)
+        potega *= e;
+
+        if (potega > b)
+        return d;
+    }
+}
